4ee5ca12fd0562b49a8619d8bf54ef1c8507969a
Amish <amish.tufail2002@gmail.com>
2023-12-29 21:22:39 +0500

    Initial Commit

---
+++ SwiftUIBootCamp/SwiftUIBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootCampApp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Amish Tufail on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
