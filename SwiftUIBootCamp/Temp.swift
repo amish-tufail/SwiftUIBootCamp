2f370a9b48c4da4ce8f1232fddbecf74ecc8bea6
Amish <amish.tufail2002@gmail.com>
2024-01-01 22:19:49 +0500

    Added Temp File

---
+++ SwiftUIBootCamp/Temp.swift
@@ -0,0 +1,18 @@
+//
+//  Temp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Amish Tufail on 01/01/2024.
+//
+
+import SwiftUI
+
+struct Temp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Temp()
+}
