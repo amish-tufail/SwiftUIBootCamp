4be9ef74ad487455a5637ed0d74be998fbe6c591
bisckoot <100939251+amish-tufail@users.noreply.github.com>
2024-03-25 06:52:39 +0500

    - Threads

--- SwiftUIBootCamp/Intermediate/ThreadsBootCamp.swift
+++ SwiftUIBootCamp/Intermediate/ThreadsBootCamp.swift
@@ -35,8 +35,24 @@ class BackThreadVM: ObservableObject {
     @Published var dataArray: [String] = []
     
     func fetchData() {
-        let newData = downloadData()
-        dataArray = newData
+        DispatchQueue.global().async { // background thread
+//            DispatchQueue.global(qos: .background).async { // More options
+            let newData = self.downloadData()
+            print(Thread.isMainThread)
+            print(Thread.current)
+            DispatchQueue.main.async { // main thread
+                self.dataArray = newData
+                print(Thread.isMainThread)
+                print(Thread.current)
+            }
+            /*
+             false
+             <NSThread: 0x60000174e240>{number = 7, name = (null)}
+             -----------------------------------------------------
+             true
+             <_NSMainThread: 0x600001700040>{number = 1, name = main}
+            */
+        }
     }
     private func downloadData() -> [String] {
         var data: [String] = []
