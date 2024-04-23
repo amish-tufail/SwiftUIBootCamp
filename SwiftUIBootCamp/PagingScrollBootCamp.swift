a8b6c7e9f54730f6e44c0aa612e02536df124f75
Amish <amish.tufail2002@gmail.com>
2024-04-23 20:29:40 +0500

    2. Page Scroll

--- SwiftUIBootCamp/PagingScrollBootCamp.swift
+++ SwiftUIBootCamp/PagingScrollBootCamp.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct PagingScrollBootCamp: View {
+    @State private var scrollPosition: Int? = nil
     var body: some View {
         ScrollView {
-            VStack(spacing: 30.0) {
+            VStack {
                 ForEach(0..<10) { index in
                     Rectangle()
                         .frame(width: 300.0, height: 300.0)
@@ -19,7 +20,14 @@ struct PagingScrollBootCamp: View {
                                 .foregroundStyle(.white)
                         }
                         .frame(maxWidth: .infinity)
+                        .padding()
                         .containerRelativeFrame(.vertical, alignment: .center)
+                        .id(index)
+                        .scrollTransition(.interactive) { content, phase in
+                            content
+                                .opacity(phase.isIdentity ? 1.0 : 0.0)
+                                .blur(radius: phase.isIdentity ? 0.0 : 20.0)
+                        }
                 }
             }
         }
@@ -27,6 +35,15 @@ struct PagingScrollBootCamp: View {
 //        .scrollTargetBehavior(.viewAligned) // aligns to a edge
         .scrollTargetBehavior(.paging)
         .scrollBounceBehavior(.basedOnSize) // scroll enables based on size, if size less no scroll, as data is more scroll enables
+        .scrollPosition(id: $scrollPosition, anchor: .center)
+        .animation(.smooth, value: scrollPosition)
+        .safeAreaInset(edge: .bottom) {
+            Button {
+                scrollPosition = (0..<10).randomElement()
+            } label: {
+                Text("Scroll To")
+            }
+        }
     }
 }
 
