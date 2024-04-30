d91a1d040e8f8ba4a044849a24c67cfe79376ca4
Amish <amish.tufail2002@gmail.com>
2024-04-30 19:36:11 +0500

    - core data complete

--- SwiftUIBootCamp/CoreData/Relationships/CoreDataRelationshipBootCamp.swift
+++ SwiftUIBootCamp/CoreData/Relationships/CoreDataRelationshipBootCamp.swift
@@ -15,7 +15,7 @@ struct CoreDataRelationshipBootCamp: View {
             ScrollView {
                 VStack(spacing: 20.0) {
                     Button {
-                        vm.addDepartment()
+                        vm.deleteDepartment()
                     } label: {
                         Text("Perform Action")
                             .foregroundStyle(.white)
@@ -38,6 +38,14 @@ struct CoreDataRelationshipBootCamp: View {
                         HStack(alignment: .top) {
                             ForEach(vm.departments) { department in
                                 DepartmentView(entity: department)
+                            }
+                        }
+                    }
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(alignment: .top) {
+                            ForEach(vm.employees) { employee in
+                                EmployeeView(entity: employee)
                             }
                         }
                     }
@@ -65,7 +73,7 @@ struct BusinessView: View {
                     .bold()
                 ForEach(departments) { department in
                     Text(department.name ?? "")
-                        .bold()
+                        .font(.caption)
                 }
             }
             
@@ -74,7 +82,7 @@ struct BusinessView: View {
                     .bold()
                 ForEach(employees) { employee in
                     Text(employee.name ?? "")
-                        .bold()
+                        .font(.caption)
                 }
             }
         }
@@ -94,10 +102,11 @@ struct DepartmentView: View {
                 .font(.title3)
                 .bold()
             if let businesses = entity.business?.allObjects as? [DepartmentEntity] {
-                Text("Departments:")
+                Text("Businesses:")
                     .bold()
                 ForEach(businesses) { business in
                     Text(business.name ?? "")
+                        .font(.caption)
                 }
             }
             
@@ -106,12 +115,48 @@ struct DepartmentView: View {
                     .bold()
                 ForEach(employees) { employee in
                     Text(employee.name ?? "")
+                        .font(.caption)
                 }
             }
         }
         .padding()
         .frame(maxWidth: 300.0, alignment: .leading)
         .background(.green.opacity(0.5))
+        .clipShape(.rect(cornerRadius: 10.0))
+        .shadow(radius: 10.0)
+    }
+}
+
+struct EmployeeView: View {
+    let entity: EmployeeEntity
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20.0) {
+            Text("Name: \(entity.name ?? "")")
+                .font(.title3)
+                .bold()
+            Text("Age: \(entity.age)")
+                .font(.title3)
+                .bold()
+            Text("Date Joined: \(entity.dateJoined ?? Date())")
+                .font(.title3)
+                .bold()
+            if let business = entity.business {
+                Text("Business:")
+                    .bold()
+                Text(business.name ?? "")
+                    .font(.caption)
+            }
+            
+            if let department = entity.department {
+                Text("Department:")
+                    .bold()
+                Text(department.name ?? "")
+                    .font(.caption)
+            }
+        }
+        .padding()
+        .frame(maxWidth: 300.0, alignment: .leading)
+        .background(.blue.opacity(0.5))
         .clipShape(.rect(cornerRadius: 10.0))
         .shadow(radius: 10.0)
     }
--- SwiftUIBootCamp/CoreData/Relationships/CoreDataRelationshipViewModel.swift
+++ SwiftUIBootCamp/CoreData/Relationships/CoreDataRelationshipViewModel.swift
@@ -18,12 +18,20 @@ class CoreDataRelationshipViewModel: ObservableObject {
     init() {
         getBusinessess()
         getDepartments()
+        getEmployees()
     }
     
     
     // Business
     func getBusinessess() {
         let request = NSFetchRequest<BusinessEntity>(entityName: "BusinessEntity")
+        // Sorting
+        let sort = NSSortDescriptor(keyPath: \BusinessEntity.name, ascending: true)
+        request.sortDescriptors = [sort]
+        
+        // Filtering
+        let filter = NSPredicate(format: "name == %@", "Apple")
+        request.predicate = filter
         do {
             businesses = try manager.context.fetch(request)
         } catch let error {
@@ -32,11 +40,11 @@ class CoreDataRelationshipViewModel: ObservableObject {
     }
     func addBusiness() {
         let newBusiness = BusinessEntity(context: manager.context)
-        newBusiness.name = "Apple"
+        newBusiness.name = "Google"
         // add exisiting departments to the new business
-//        newBusiness.departments = []
-        // add exisiting employees to the new business
-//        newBusiness.employees = []
+        newBusiness.departments = [departments[0], departments[1]]
+//        // add exisiting employees to the new business
+//        newBusiness.employees = [employees[0], employees[1]]
         
         // add new business to the the existing departments
 //        newBusiness.addToDepartments(<#T##value: DepartmentEntity##DepartmentEntity#>)
@@ -44,7 +52,11 @@ class CoreDataRelationshipViewModel: ObservableObject {
 //        newBusiness.addToEmployees(<#T##value: EmployeeEntity##EmployeeEntity#>)
         save()
     }
-    
+    func updateBusiness() {
+        let exisitingBusiness = businesses[1]
+        exisitingBusiness.departments = [departments[0], departments[1]]
+        save()
+    }
     // Department
     func getDepartments() {
         let request = NSFetchRequest<DepartmentEntity>(entityName: "DepartmentEntity")
@@ -56,8 +68,10 @@ class CoreDataRelationshipViewModel: ObservableObject {
     }
     func addDepartment() {
         let newDepartment = DepartmentEntity(context: manager.context)
-        newDepartment.name = "Engineering"
+        newDepartment.name = "Marketing"
         newDepartment.business = [businesses[0]]
+        newDepartment.employees = [employees[1]]
+//        newDepartment.addToEmployees(employees[1]) // same as above
         save()
     }
     
@@ -72,20 +86,44 @@ class CoreDataRelationshipViewModel: ObservableObject {
     }
     func addEmployee() {
         let newEmployee = EmployeeEntity(context: manager.context)
-        newEmployee.name = "Amish"
-        newEmployee.age = 22
+        newEmployee.name = "Mohsin"
+        newEmployee.age = 20
         newEmployee.dateJoined = Date()
+        newEmployee.business = businesses[0]
+        newEmployee.department = departments[0]
         save()
     }
+    func getEmployees(forBusiness business: BusinessEntity) {
+        let request = NSFetchRequest<EmployeeEntity>(entityName: "EmployeeEntity")
+        let filter = NSPredicate(format: "business == %@", business)
+        request.predicate = filter
+        do {
+            employees = try manager.context.fetch(request)
+        } catch let error {
+            print("Error fetching businesses: \(error)")
+        }
+    }
+    
+    // Delete
+    func deleteDepartment() {
+        let department = departments[0]
+        manager.context.delete(department)
+        save()
+    } // Department entity -> inside it relatioship in which we add delete rule, so if delete rule is deny, then if we delete a department then a relartionship which has that rule will prevent it from happening
+
     
     // Save function
     func save() {
         businesses.removeAll()
         departments.removeAll()
+        employees.removeAll()
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.manager.save()
             self.getBusinessess()
             self.getDepartments()
+            self.getEmployees()
         }
     }
 }
+
+
